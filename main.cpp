b6b15df099b90fa9689e6c4a6d5006a076241854
wermos <63574588+wermos@users.noreply.github.com>
2022-03-31 01:40:07 +0530

    Revamped the main function to get the data and graphs we need.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <cstddef> // for the std::size_t data type
 #include <array> // for std::array
+#include <cmath> // for std::fabs
+#include <iostream>
 
 #include "boost/numeric/odeint.hpp" // for testing purposes
 
@@ -15,7 +17,7 @@ typedef std::array<double, 6> StateType;
 // originally, this was a boost::array, but then Boost documentation itself
 // recommended using std:array if the project is written in C++11 or higher.
 
-constexpr std::size_t numSteps = 2'0;
+constexpr std::size_t numSteps = 40'000;
 constexpr double speed_of_light = 299'792'458; // units: m/s
 double mass = 9.109e-31; // units: kg
 double charge = 1.602e-19; // units: C
@@ -86,6 +88,18 @@ std::vector<StateType> boostResult(const StateType initialState, const double t0
 	return values;
 }
 
+vec3 getPosition(StateType state) {
+	// The objective is to print the position vector, given the values.
+	// To achieve this, we just convert the raw numbers into a vec3 because
+	// that class has opertor<< overloaded.
+	return {state[0], state[1], state[2]};
+}
+
+double getMomentum(StateType state) {
+	// A helper function for getting the momentum from the state, for Boost
+	return state[3] * state[3] + state[4] * state[4] + state[5] * state[5];
+}
+
 int main() {
 	namespace plt = matplotlibcpp;
 
@@ -107,56 +121,108 @@ int main() {
 	std::vector<State> values = Solver::RK4(initialState, t0, tStep, numSteps, E, B);
 	// obtaining the values using my own implementation of the Boris leapfrog algorithm
 	std::vector<State> leapFrogValues = Solver::LeapFrog(initialState, t0, tStep, numSteps, E, B);
-
+	// obtaining the values using Boost 
 	std::vector<StateType> boostValues = boostResult(initialStateBoost, t0, tStep, numSteps);
 
-	// Getting the times for which we have the X and Y values
-	std::vector<double> times;
-	times.reserve(numSteps + 1);
 
-	for (auto i = 0; i < numSteps + 1; ++i) {
-		times.push_back(t0 + i * tStep);
+	for (auto i = 1; i < leapFrogValues.size(); ++i) {
+		// We start at i = 1 to avoid the false negative at i = 0
+		double posX = leapFrogValues[i].getPosition().x();
+		double posY = leapFrogValues[i].getPosition().y();
+		double posZ = leapFrogValues[i].getPosition().z();
+		if (std::fabs(posX) < 1e-5 && std::fabs(posY) < 1e-5 && std::fabs(posZ) < 1e-5) {
+			// The idea is that, the particle starts at the origin. If we see the time taken
+			// for it to come back to the origin (allowing for some error due to loss in
+			// precision), this will be the time period.
+			std::cout << "Time period: " << i * tStep << '\n';
+			break;
+		}
 	}
 
-	std::vector<double> rk4_X, rk4_Y;
-	rk4_X.reserve(numSteps + 1);
-	rk4_Y.reserve(numSteps + 1);
-	for (auto& elem: values) {
-		rk4_X.push_back(elem.getPosition().x());
-		rk4_Y.push_back(elem.getPosition().y());
-		//std::cout << elem << '\n';
+	double radius; // This will store the radius of curvature of the circle
+	for (auto i = 0; i < leapFrogValues.size(); ++i) {
+		double posX = leapFrogValues[i].getPosition().x();
+		double posY = leapFrogValues[i].getPosition().y();
+		if (std::fabs(posX) < 1e-5 && std::fabs(posY) >= 1e-5) {
+			// The electron is travelling in the XY plane, and so it will go through (0, 0) and
+			// (0, 2 * r). Allowing for some precision issues, this is what we are checking for in
+			// the if clause: When is the x coordinate approximately 0, but the y coordinate is
+			// non-zero?
+			radius = std::fabs(posY) / 2; // because radius cannot be negative
+			break;
+		}
 	}
+	std::cout << "Radius of curvature: " << radius << "\n\n";
 
-	std::vector<double> leapFrog_X, leapFrog_Y;
-	leapFrog_X.reserve(numSteps + 1);
-	leapFrog_Y.reserve(numSteps + 1);
-	for (auto& elem: leapFrogValues) {
-		leapFrog_X.push_back(elem.getPosition().x());
-		leapFrog_Y.push_back(elem.getPosition().y());
-		//std::cout << elem << '\n';
-	}
+	std::cout << "Position after 10 turns:\n";
+	std::cout << "RK4:\t\t(" << values[10].getPosition() << ")\n";
+	std::cout << "Leapfrog:\t(" << leapFrogValues[10].getPosition() << ")\n";
+	std::cout << "Boost:\t\t(" << getPosition(boostValues[10]) << ")\n\n";
 
-	std::vector<double> boostX, boostY;
-	boostX.reserve(numSteps + 1);
-	boostY.reserve(numSteps + 1);
-	for (auto& elem: boostValues) {
-		boostX.push_back(elem[0]); // x-coordinate
-		boostY.push_back(elem[1]); // y-coordinate
+	std::cout << "Position after 100 turns:\n";
+	std::cout << "RK4:\t\t(" << values[100].getPosition() << ")\n";
+	std::cout << "Leapfrog:\t(" << leapFrogValues[100].getPosition() << ")\n";
+	std::cout << "Boost:\t\t(" << getPosition(boostValues[100]) << ")\n\n";
+
+	std::cout << "Position after 1,000 turns:\n";
+	std::cout << "RK4:\t\t(" << values[1'000].getPosition() << ")\n";
+	std::cout << "Leapfrog:\t(" << leapFrogValues[1'000].getPosition() << ")\n";
+	std::cout << "Boost:\t\t(" << getPosition(boostValues[1'000]) << ")\n\n";
+
+	std::cout << "Position after 10,000 turns:\n";
+	std::cout << "RK4:\t\t(" << values[10'000].getPosition() << ")\n";
+	std::cout << "Leapfrog:\t(" << leapFrogValues[10'000].getPosition() << ")\n";
+	std::cout << "Boost:\t\t(" << getPosition(boostValues[10'000]) << ")\n\n";
+
+	std::cout << "Momentum after 10 turns:\n";
+	std::cout << "RK4:\t\t" << values[10].getMomentum().length() << "\n";
+	std::cout << "Leapfrog:\t" << leapFrogValues[10].getMomentum().length() << "\n";
+	std::cout << "Boost:\t\t" << getMomentum(boostValues[10]) << "\n\n";
+
+	std::cout << "Momentum after 100 turns:\n";
+	std::cout << "RK4:\t\t" << values[100].getMomentum().length() << "\n";
+	std::cout << "Leapfrog:\t" << leapFrogValues[100].getMomentum().length() << "\n";
+	std::cout << "Boost:\t\t" << getMomentum(boostValues[100]) << "\n\n";
+
+	std::cout << "Momentum after 1,000 turns:\n";
+	std::cout << "RK4:\t\t" << values[1'000].getMomentum().length() << "\n";
+	std::cout << "Leapfrog:\t" << leapFrogValues[1'000].getMomentum().length() << "\n";
+	std::cout << "Boost:\t\t" << getMomentum(boostValues[1'000]) << "\n\n";
+
+	std::cout << "Momentum after 10,000 turns:\n";
+	std::cout << "RK4:\t\t" << values[10'000].getMomentum().length() << "\n";
+	std::cout << "Leapfrog:\t" << leapFrogValues[10'000].getMomentum().length() << "\n";
+	std::cout << "Boost:\t\t" << getMomentum(boostValues[10'000]) << "\n\n";
+
+	constexpr double trueMomentum = 2.458e-22;
+
+	std::vector<std::size_t> numTurns;
+	numTurns.reserve(10'000);
+	for (auto i = 0; i <= 10'000; ++i) {
+		numTurns.push_back(i);
 	}
  
+	std::vector<double> boostMomentum, leapFrogMomentum, RK4Momentum;
+	boostMomentum.reserve(10'001);
+	leapFrogMomentum.reserve(10'001);
+	RK4Momentum.reserve(10'001);
+	for (auto i = 0; i <= 40'000; i += 4) {
+		boostMomentum.push_back(std::fabs(trueMomentum - getMomentum(boostValues[i])));
+		leapFrogMomentum.push_back(std::fabs(trueMomentum - leapFrogValues[i].getMomentum().length()));
+		RK4Momentum.push_back(std::fabs(trueMomentum - values[i].getMomentum().length()));
+	}
+
 	plt::figure_size(1280, 720);
 
-	plt::named_plot("RK4", times, rk4_X, "r-");
-	plt::plot(times, rk4_Y, "r-");
+	plt::named_plot("RK4", numTurns, RK4Momentum, "r-");
 
-	plt::named_plot("Boris Leap frog", times, leapFrog_X, "b-");
-	plt::plot(times, leapFrog_Y, "b-");
+	plt::named_plot("Boris Leap frog", numTurns, leapFrogMomentum, "b-");
 	
-	//plt::named_plot("Boost Adams-Bashforth-Moulton", times, boostX, "k-");
-	//plt::plot(times, boostY, "k-");
+	//plt::named_plot("Boost Adams-Bashforth-Moulton", numTurns, boostMomentum, "k-");
 
-	plt::title("Position w.r.t. t"); // Add graph title
+	plt::title("Deviation of Momentum w.r.t. number of turns"); // Add graph title
 
 	plt::legend(); // Enable legend.
 	plt::show();
+
 }
