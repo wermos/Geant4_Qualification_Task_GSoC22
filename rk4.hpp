4aff551a965c5422f8701d3365b0de279948dfec
wermos <63574588+wermos@users.noreply.github.com>
2022-03-30 11:27:40 +0530

    Ironed out the kinks in the functions and fixed all the bugs.

--- rk4.hpp
+++ rk4.hpp
@@ -1,8 +1,14 @@
 #ifndef RK4_HPP
 #define RK4_HPP
 
-#include <cstdlib> // for the std::size_t data type
+#include <cstddef> // for the std::size_t data type
+#include <array> // for std::array
 #include <vector>
+
+#ifdef __cpp_lib_concepts
+	#include <concepts> // for std::same_as
+#endif
+
 #include "state.hpp"
 #include "vec3.hpp"
 
@@ -14,20 +20,64 @@ extern double charge;
 namespace Solver {
 #ifdef __cpp_lib_concepts
 	template <typename FunctionType>
-	concept 3DFunction = requires (FunctionType func, double t) {
-		func(t) -> double*;
+	concept EMFunc = requires (FunctionType func, const double t) {
+		{ func(t) } -> std::same_as<std::array<double, 3>>;
 	};
 #endif
 
 #ifdef __cpp_lib_concepts
-	template <3DFunction Callable>
+	template <typename Callable> requires EMFunc<Callable>
+#else
+	template <typename Callable>
+#endif
+	State functionEvaluator(const State& currentState, const double t, Callable EFunc, Callable BFunc) {
+		// Query the function which returns the value of E for the current value
+		// of E and store it inside EField
+		auto EField = EFunc(t);
+
+		// Query the function which returns the value of B for the current value
+		// of B and store it inside BField
+		auto BField = BFunc(t);
+
+		auto v = currentState.getMomentum() / mass;
+		// converting the momentum vector into a velocity vector
+
+		vec3 totalForce = charge * (EField + vec3::cross(v, BField));	
+
+		State newState;
+
+		newState.setPosition(v);
+		newState.setMomentum(totalForce);
+
+		return newState;
+	}
+
+#ifdef __cpp_lib_concepts
+	template <typename Callable> requires EMFunc<Callable>
+#else
+	template <typename Callable>
+#endif
+	State RKStepper(const State& currentState, const double t, const double tStep,
+			Callable EFunc, Callable BFunc) {
+		State k1 = functionEvaluator(currentState, t, EFunc, BFunc);
+		State k2 = functionEvaluator(currentState + (tStep / 2) * k1, t + (tStep / 2), EFunc, BFunc);
+		State k3 = functionEvaluator(currentState + (tStep / 2) * k2, t + (tStep / 2), EFunc, BFunc);
+		State k4 = functionEvaluator(currentState + tStep * k3, t + tStep, EFunc, BFunc);
+
+		State tempState = currentState + (1.0 / 6) * tStep * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
+		return tempState;
+	}
+
+#ifdef __cpp_lib_concepts
+	template <typename Callable> requires EMFunc<Callable>
 #else
 	template <typename Callable>
 #endif
 	std::vector<State> RK4(const State initialState, const double t0,
 			const double tStep, const std::size_t numSteps, Callable EFunc,
 			Callable BFunc) {
-		std::vector<State> values(numStep + 1);
+		std::vector<State> values;
+		values.reserve(numSteps + 1);
 		// explicitly initialize the vector with the required amount of space to
 		// prevent memory reallocations
 
@@ -37,63 +87,11 @@ namespace Solver {
 
 		for (auto i = 0; i < numSteps; ++i) {
 			State state = RKStepper(values.back(), currentTime, tStep, EFunc, BFunc);
-			values.push_back(state)
+			values.push_back(state);
 			currentTime += tStep;
 		}
 
 		return values;
 	}
-
-#ifdef __cpp_lib_concepts
-	template <3DFunction Callable>
-#else
-	template <typename Callable>
-#endif
-	State RKStepper(const State currentState, const double t, const double tStep,
-			Callable EFunc, Callable BFunc) {
-		State k1 = functionEvaluator(currentState, t, EFunc, BFunc);
-		State k2 = functionEvaluator(currentState + (tStep / 2) * k1, t + (tStep / 2), EFunc, BFunc);
-		State k3 = functionEvaluator(currentState + (tStep / 2) * k2, t + (tStep / 2), EFunc, BFunc);
-		State k4 = functionEvaluator(currentState + tStep * k3, t + tStep, EFunc, BFunc);
-
-		return currentState + (1.0 / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
-	}
-
-#ifdef __cpp_lib_concepts
-	template <3DFunction Callable>
-#else
-	template <typename Callable>
-#endif
-	State functionEvaluator(const State& currentState, const double t, Callable EFunc, Callable BFunc) {
-		// Query the function which returns the value of E for the current current
-		// value of E and store it inside EField
-		double EField[3];
-		EFunc(t, EField);
-
-		// Query the function which returns the value of B for the current current
-		// value of B and store it inside BField
-		double BField[3];
-		BFunc(t, BField);
-
-		/**
-		// Slicing the currentState array into position and momentum vec3 variables
-		vec3 x(currentState[0]);
-		vec3 p(currentState[3]);
-		vec3 v(p); // copying the contents of the momentum vector into the velocity vector
-
-		v /= mass; // converting the momentum vector into a velocity vector
-		*/
-
-		vec3 totalForce = charge * (EField + vec3::cross(v, BField));		
-		//totalForce /= mass
-
-		State newState;
-
-		newState.setPosition(currentState.getPosition());
-		newState.setMomentum(totalForce);
-
-		return newState;
-	}
 }
-
 #endif // RK4_HPP
--- state.hpp
+++ state.hpp
@@ -5,31 +5,38 @@
 
 class State {
 	public:
-		constexpr State() noexcept : y({0, 0, 0, 0, 0, 0}) {}
-		constexpr State(double (&arr)[6]) noexcept :
-			arr({-arr[0], -arr[1], -arr[2], -arr[3], -arr[4], -arr[5]}) {}
+		constexpr State() noexcept : y{0, 0, 0, 0, 0, 0} {}
+		constexpr State(const double y0, const double y1,
+						const double y2, const double y3,
+						const double y4, const double y5) noexcept :
+			y{y0, y1, y2, y3, y4, y5} {}
+		constexpr State(const double* arr) noexcept :
+			y{arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]} {}
+		constexpr State(const vec3& x, const vec3& p) noexcept :
+			y{x[0], x[1], x[2], p[0], p[1], p[2]} {}
 		constexpr State(const State&) noexcept = default;
 
 		constexpr vec3 getPosition() const {
 			// TODO: Add comment explaining why this works
-			return {y[0]};
+			return {&y[0]};
 		}
 
 		constexpr vec3 getMomentum() const {
 			// TODO: Add comment explaining why this works
-			return {y[3]};
+			return {&y[3]};
 		}
 
 		constexpr void setPosition(const vec3& x) {
-			x.toArray(y[0]);
+			x.toArray(&y[0]);
 		}
 
 		constexpr void setMomentum(const vec3& p) {
-			p.toArray(y[3]);
+			p.toArray(&y[3]);
 		}
 
-		constexpr void toArray(double (&out)[3]) const {
-			for (auto i = 0; i < 3; ++i) {
+		constexpr void toArray(double* out) const {
+			// Undefined behavior if the array is too small
+			for (auto i = 0; i < 6; ++i) {
 				out[i] = y[i];
 			}
 		}
@@ -46,48 +53,72 @@ class State {
 			return y[i];
 		}
 
-		friend constexpr State& operator+=(State& u, const State& v) {
-			u.y[0] += v.y[0];
-			u.y[1] += v.y[1];
-			u.y[2] += v.y[2];
-			return u;
+		friend constexpr State& operator+=(State& s1, const State& s2) {
+			s1.y[0] += s2.y[0];
+			s1.y[1] += s2.y[1];
+			s1.y[2] += s2.y[2];
+			s1.y[3] += s2.y[3];
+			s1.y[4] += s2.y[4];
+			s1.y[5] += s2.y[5];
+			return s1;
 		}
 
-		friend constexpr State& operator-=(State& u, const State& v) {
-			u.y[0] -= v.y[0];
-			u.y[1] -= v.y[1];
-			u.y[2] -= v.y[2];
-			return u;
+		friend constexpr State& operator-=(State& s1, const State& s2) {
+			s1.y[0] -= s2.y[0];
+			s1.y[1] -= s2.y[1];
+			s1.y[2] -= s2.y[2];
+			s1.y[3] -= s2.y[3];
+			s1.y[4] -= s2.y[4];
+			s1.y[5] -= s2.y[5];
+			return s1;
 		}
 
-		friend constexpr State& operator*=(State& v, const double t) {
-			v.y[0] *= t;
-			v.y[1] *= t;
-			v.y[2] *= t;
-			return v;
+		friend constexpr State& operator*=(State& s, const double t) {
+			s.y[0] *= t;
+			s.y[1] *= t;
+			s.y[2] *= t;
+			s.y[3] *= t;
+			s.y[4] *= t;
+			s.y[5] *= t;
+			return s;
 		}
 
-		friend constexpr State& operator*=(const double t, State& v) {
-			return v *= t;
+		friend constexpr State& operator*=(const double t, State& s) {
+			return s *= t;
 		}
 
-		friend constexpr State& operator/=(State& v, const double t) {
-			return v *= (1 / t);
+		friend constexpr State& operator/=(State& s, const double t) {
+			return s *= (1 / t);
 		}
 
-		friend constexpr State operator+(const State& u, const State& v) {
-			State copy = u;
-			copy += v;
+		friend constexpr State operator+(const State& s1, const State& s2) {
+			State copy = s1;
+			copy += s2;
 			return copy;
 		}
 
-		friend constexpr State operator-(const State& u, const State& v) {
-			State copy = u;
-			copy -= v;
+		friend constexpr State operator-(const State& s1, const State& s2) {
+			State copy = s1;
+			copy -= s2;
+			return copy;
+		}
+
+		friend constexpr State operator*(const State& s, const double t) {
+			State copy = s;
+			copy *= t;
+			return copy;
+		}
+
+		friend constexpr State operator*(const double t, const State& s) {
+			State copy = s;
+			copy *= t;
+			return copy;
 		}
 
 		friend std::ostream& operator<<(std::ostream& out, const State& s) {
-			out << y.y[0] << " " << y.y[1] << " " << y.y[2];
+			for (auto i = 0; i < 6; i++) {
+				out << s.y[i] << " ";
+			}
 			return out;
 		}
 
